1b82212c2a9b0ca258955a0f03c025b4417b2788
Karl Royer <karl@bigmight.com>
2026-06-04 12:21:19 +0100

    Changes to add new tests for expired certificate, and foreign cert and wrong key

--- acceptance_tests/spec/eap_tls_spec.rb
+++ acceptance_tests/spec/eap_tls_spec.rb
@@ -18,9 +18,26 @@ describe "EAP TLS" do
       expect(run_eapoltest.call).to have_been_successful
     end
   end
-  context "Unsuccessful" do
-    let(:client_cert_path) { "/usr/src/app/certs/invalid_client.pem" }
-    let(:client_key_path) { "/usr/src/app/certs/invalid_client.key" }
+
+  context "Unsuccessful - wrong key" do
+    let(:client_cert_path) { "/usr/src/app/certs/client.pem" }
+    let(:client_key_path) { "/usr/src/app/certs/foreign_client.key" }
+    it "fails" do
+      expect(run_eapoltest.call).to have_failed
+    end
+  end
+
+  context "Unsuccessful - expired client certificate" do
+    let(:client_cert_path) { "/usr/src/app/certs/expired_client.pem" }
+    let(:client_key_path) { "/usr/src/app/certs/expired_client.key" }
+    it "fails" do
+      expect(run_eapoltest.call).to have_failed
+    end
+  end
+
+  context "Unsuccessful - foreign client certificate" do
+    let(:client_cert_path) { "/usr/src/app/certs/foreign_client.pem" }
+    let(:client_key_path) { "/usr/src/app/certs/foreign_client.key" }
     it "fails" do
       expect(run_eapoltest.call).to have_failed
     end
