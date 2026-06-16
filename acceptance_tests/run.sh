b36fdbdfd1e9fe2869f19ecfd5b71f1105e319dd
Karl Royer <karl@bigmight.com>
2026-06-16 15:19:51 +0100

    Add comments regarding test run order

--- acceptance_tests/run.sh
+++ acceptance_tests/run.sh
@@ -6,5 +6,7 @@ rm /test_log_data/*
 # Copy cert into place
 aws --endpoint-url=${ENDPOINT_URL} s3 cp ${CERT_STORE_BUCKET}/ /usr/src/app/certs/ --recursive
 
-# Run tests
+# Run tests - it is important they are run in a predetermined order (filesystem default sort)
+# to ensure the logfile test is run after the eap testing has completed. The "--order defined"
+# option ensured this happens
 bundle exec rspec --order defined -f d
