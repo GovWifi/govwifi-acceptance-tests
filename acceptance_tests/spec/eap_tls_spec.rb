0f5fc4edf60d4d30f4e503c34e0c4078a981aaf8
camdesgov <49409388+camdesgov@users.noreply.github.com>
2026-06-16 16:02:05 +0100

    Revert "Add invalif client back in"
    This reverts commit 8fe93f6f04acbed87a8957a6fb6f3e961c0f2104.

--- acceptance_tests/spec/eap_tls_spec.rb
+++ acceptance_tests/spec/eap_tls_spec.rb
@@ -21,14 +21,7 @@ describe "EAP TLS" do
 
   context "Unsuccessful - wrong key" do
     let(:client_cert_path) { "/usr/src/app/certs/client.pem" }
-    let(:client_key_path)  { "/usr/src/app/certs/foreign_client.key" }
-    it "fails" do
-      expect(run_eapoltest.call).to have_failed
-    end
-
-  context "Unsuccessful - wrong key" do
-    let(:client_cert_path) { "/usr/src/app/certs/invalid_client.pem" }
-    let(:client_key_path)  { "/usr/src/app/certs/invalid_client.key" }
+    let(:client_key_path) { "/usr/src/app/certs/foreign_client.key" }
     it "fails" do
       expect(run_eapoltest.call).to have_failed
     end
@@ -36,7 +29,7 @@ describe "EAP TLS" do
 
   context "Unsuccessful - expired client certificate" do
     let(:client_cert_path) { "/usr/src/app/certs/expired_client.pem" }
-    let(:client_key_path)  { "/usr/src/app/certs/expired_client.key" }
+    let(:client_key_path) { "/usr/src/app/certs/expired_client.key" }
     it "fails" do
       expect(run_eapoltest.call).to have_failed
     end
@@ -44,7 +37,7 @@ describe "EAP TLS" do
 
   context "Unsuccessful - foreign client certificate" do
     let(:client_cert_path) { "/usr/src/app/certs/foreign_client.pem" }
-    let(:client_key_path)  { "/usr/src/app/certs/foreign_client.key" }
+    let(:client_key_path) { "/usr/src/app/certs/foreign_client.key" }
     it "fails" do
       expect(run_eapoltest.call).to have_failed
     end
